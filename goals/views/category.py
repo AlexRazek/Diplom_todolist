4905a6d3924e73de9ff76ac8aeca288bf6d64284
alexander <Avvrazek@gmail.com>
2022-07-08 14:23:27 +0300

    new changes for bot

--- goals/views/category.py
+++ goals/views/category.py
@@ -3,7 +3,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated
 
 from goals.models import GoalCategory, Goal
 from goals.permissionts import GoalCategoryPermissions
