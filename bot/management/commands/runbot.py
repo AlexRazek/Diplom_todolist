d0074c6f536dfb8b8c20f39ff0b9feb6b77ad27b
alexander <Avvrazek@gmail.com>
2022-07-08 17:16:23 +0300

    hw38_step_9

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -5,6 +5,7 @@ from django.core.management import BaseCommand
 from bot.models import TgUser
 from bot.tg.client import TgClient
 from bot.tg.dc import Message
+from goals.models import Goal
 
 from todolist import settings
 
@@ -28,6 +29,19 @@ class Command(BaseCommand):
             text=f'[verification code] {code}'
         )
 
+    def handle_goal_list(self, msg: Message, tg_user: TgUser):
+        resp_goals: list[str] = [
+            f'#{goal.id} {goal.title}'
+            for goal in Goal.objects.filter(user_id=tg_user.user_id)
+        ]
+        self.tg_client.send_message(msg.chat.id, '\n'.join(resp_goals) or '[goals не найдены]')
+
+    def handle_verified_user(self, msg: Message, tg_user: TgUser):
+        if msg.text == '/goals':
+            self.handle_goal_list(msg=msg, tg_user=tg_user)
+        elif msg.text.startswith('/'):
+            self.tg_client.send_message(msg.chat.id, '[Неизвестная команда]')
+
     def handle_message(self, msg: Message):
         tg_user, created = TgUser.objects.get_or_create(
             chat_id=msg.chat.id,
@@ -40,7 +54,7 @@ class Command(BaseCommand):
         elif not tg_user.user:
             self.handle_user_without_verification(msg=msg, tg_user=tg_user)
         else:
-            ...
+            self.handle_verified_user(msg=msg, tg_user=tg_user)
 
     def handle(self, *args, **options):
         offset = 0
@@ -48,6 +62,6 @@ class Command(BaseCommand):
             res = self.tg_client.get_updates(offset=offset)
             for item in res.result:
                 offset = item.update_id + 1
-                print(item.message)
+                # print(item.message)
                 self.handle_message(msg=item.message)
                 # self.tg_client.send_message(chat_id=item.message.chat.id, text=item.message.text)
