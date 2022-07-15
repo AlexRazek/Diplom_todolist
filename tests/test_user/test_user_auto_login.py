70a6e27ff88af2ba439af703199046cb93228ad7
alexander <Avvrazek@gmail.com>
2022-07-15 16:21:32 +0300

    hw39_tests_additional

---
+++ tests/test_user/test_user_auto_login.py
@@ -0,0 +1,10 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_auth_view(auto_login_user):
+    client, user = auto_login_user()
+    url = reverse('core:login')
+    response = client.get(url)
+    assert response.status_code == 200
