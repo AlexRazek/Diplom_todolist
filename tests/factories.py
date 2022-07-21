54e246b50d7a7795725fd945664125da6c365d01
Vadim Meshcheryakov <painassasin@icloud.com>
2022-07-21 13:38:28 +0500

    tests

--- tests/factories.py
+++ tests/factories.py
@@ -1,8 +1,7 @@
-# from factory.django import DjangoModelFactory
 import factory.django
 
 from core.models import User
-from goals.models import Goal, Board, GoalCategory, GoalComment
+from goals.models import Board, BoardParticipant
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -17,53 +16,16 @@ class UserFactory(factory.django.DjangoModelFactory):
 
 
 class BoardFactory(factory.django.DjangoModelFactory):
+    title = factory.sequence(lambda n: f'board_title_{n}')
+
     class Meta:
         model = Board
 
-    title = factory.Faker("board_test")
+
+class BoardParticipantFactory(factory.django.DjangoModelFactory):
+    board = factory.SubFactory(BoardFactory)
     user = factory.SubFactory(UserFactory)
+    role = BoardParticipant.Role.owner
 
-
-class BoardParticipant(factory.django.DjangoModelFactory):
     class Meta:
-        model = Board
-
-    role = factory.Faker("2")
-    user = factory.SubFactory(UserFactory)
-
-
-class BoardFactoryPk(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Board
-
-    title = factory.Faker("board_pk_test")
-    participants = factory.SubFactory(BoardParticipant)
-
-
-class GoalCategoryFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = GoalCategory
-
-    board = factory.Faker("board_test")
-    title = factory.Faker("title_test")
-    user = factory.SubFactory(UserFactory)
-
-
-class GoalFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Goal
-
-    category = factory.Faker("category")
-    title = factory.Faker("title_goal")
-    user = factory.SubFactory(UserFactory)
-
-
-class CommentFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = GoalComment
-
-    goal = factory.SubFactory(GoalFactory)
-    text = factory.Faker("New moments")
-    user = factory.Faker(2)
-    category = factory.SubFactory(GoalCategoryFactory)
-    board = factory.Faker(2)
+        model = BoardParticipant
